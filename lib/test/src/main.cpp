16308f9c0affc417d12728ced21c17cc0c4a5d76
elvircn <elvircrn@gmail.com>
2017-12-24 00:15:50 +0100

    Linux filepath fix

--- lib/test/src/main.cpp
+++ lib/test/src/main.cpp
@@ -5,17 +5,16 @@
 
 using bompiler::Bompiler;
 
-int compile(const std::wstring &);
-
 TEST_CASE("Basic compile tests") {
-  REQUIRE(bompiler::Bompiler(L"data/test1.b").getState() == bompiler::Bompiler::State::SUCCESS);
-  REQUIRE(bompiler::Bompiler(L"data/test2.b").getState() == bompiler::Bompiler::State::SUCCESS);
-  REQUIRE(bompiler::Bompiler(L"data/test3.b").getState() == bompiler::Bompiler::State::SUCCESS);
-  REQUIRE(bompiler::Bompiler(L"data/test4.b").getState() == bompiler::Bompiler::State::SUCCESS);
+  REQUIRE(bompiler::Bompiler(L"data\\test1.b").getState() == bompiler::Bompiler::State::SUCCESS);
+  REQUIRE(bompiler::Bompiler(L"data\\test2.b").getState() == bompiler::Bompiler::State::SUCCESS);
+  REQUIRE(bompiler::Bompiler(L"data\\test3.b").getState() == bompiler::Bompiler::State::SUCCESS);
+  REQUIRE(bompiler::Bompiler(L"data\\test4.b").getState() == bompiler::Bompiler::State::SUCCESS);
 }
 
+
 TEST_CASE("printf test") {
-  bompiler::Bompiler compiler(L"data/printftest.b");
+  bompiler::Bompiler compiler(L"data\\printftest.b");
   REQUIRE(compiler.getState() == Bompiler::State::SUCCESS);
 }
 
