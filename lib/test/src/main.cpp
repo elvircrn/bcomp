87703ab23f647f08be7b74de8f938b8746a34603
elvircn <elvircrn@gmail.com>
2017-12-23 17:11:08 +0100

    Add broken test

--- lib/test/src/main.cpp
+++ lib/test/src/main.cpp
@@ -3,8 +3,6 @@
  */
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
-#include <catch_session.hpp>
-#include "logger.h"
 
 unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
@@ -14,5 +12,5 @@ TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( Factorial(1) == 1 );
     REQUIRE( Factorial(2) == 2 );
     REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+    REQUIRE( Factorial(10) == 36280 );
 }
