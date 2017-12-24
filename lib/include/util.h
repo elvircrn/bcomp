63597ec6199081c040fe5b13220f2fdf54df0ac2
elvircn <elvircrn@gmail.com>
2017-12-24 01:25:40 +0100

    Extract function name

---
+++ lib/include/util.h
@@ -0,0 +1,13 @@
+//
+// Created by elvircrn on 12/24/2017.
+//
+
+#ifndef BOMPILER_COREUTIL_H
+#define BOMPILER_COREUTIL_H
+
+namespace util {
+std::wstring s2ws(const std::string &str);
+std::string ws2s(const std::wstring& wstr);
+}
+
+#endif //BOMPILER_COREUTIL_H
---
+++ lib/src/util.cpp
@@ -0,0 +1,23 @@
+//
+// Created by elvircrn on 12/24/2017.
+//
+#include <string>
+#include <codecvt>
+#include <locale>
+
+#include "coreutil.h"
+
+std::wstring util::s2ws(const std::string &str) {
+  using convert_typeX = std::codecvt_utf8<wchar_t>;
+  std::wstring_convert<convert_typeX, wchar_t> converterX;
+
+  return converterX.from_bytes(str);
+}
+
+std::string util::ws2s(const std::wstring& wstr)
+{
+  using convert_typeX = std::codecvt_utf8<wchar_t>;
+  std::wstring_convert<convert_typeX, wchar_t> converterX;
+
+  return converterX.to_bytes(wstr);
+}
