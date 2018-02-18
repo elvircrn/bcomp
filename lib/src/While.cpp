8ebaadd74d087f950548f11c201785158d79a503
elvircn <elvircrn@gmail.com>
2018-02-18 14:27:43 +0100

    main edit

--- lib/src/While.cpp
+++ lib/src/While.cpp
@@ -8,7 +8,9 @@ While::While(bompiler::PNode *_parent,
              const std::vector <std::wstring> &_args) : PNode(_parent, _name, _args) {
 
 }
+#include <iostream>
 void While::setLabels(const std::wstring &_loopLabel, const std::wstring &_breakLabel) {
   loopLabel = _loopLabel;
   breakLabel = _breakLabel;
+  std::wcout << L"WOT\n";
 }
